438fb50998b3c2ea88f120c75ef792a2e360be83
Eric Jamotte <Eric.Jamotte@stib.brussels>
2021-10-15 10:57:30 +0200

    end of section 11

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryImageSize, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from '../../_services/members.service';
 
@@ -22,10 +22,8 @@ export class MemberDetailComponent implements OnInit {
 
     this.galleryOptions = [
       {
-        width: '500px',
-        height: '500px', 
-        imagePercent: 100,
-        thumbnailsColumns: 4,
+        imageSize: NgxGalleryImageSize.Contain,
+        thumbnailsColumns: 5,
         imageAnimation: NgxGalleryAnimation.Slide,
         preview: false
       }
